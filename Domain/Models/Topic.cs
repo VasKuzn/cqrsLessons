2cddf069b008b2ac72300a79c77272464ca4319c
VasKuzn <vakuznetsov_2@edu.hse.ru>
2025-04-03 18:59:34 +0500

    add: добавлена более точная работа с наполнением данных

--- Domain/Models/Topic.cs
+++ Domain/Models/Topic.cs
@@ -8,7 +8,7 @@ namespace Domain.Models
         public string TopicType { get; set; } = default!;
         public Location Location { get; set; } = default!;
 
-        public static Topic Create(TopicId id, string title, string summary, Location location, string topicType, DateTime eventStart)
+        public static Topic Create(TopicId id, string title, DateTime eventStart, string summary, string topicType, Location location)
         {
             ArgumentException.ThrowIfNullOrWhiteSpace(title);
             ArgumentException.ThrowIfNullOrWhiteSpace(summary);
